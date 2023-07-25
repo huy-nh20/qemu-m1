558662009b978c549ba04b93bfb335ee6cd9cd8c
stewie2k2 <n.hghuy1311@gmail.com>
2023-07-25 22:01:56 +0700

    Script ver 0.1.0

--- run.sh
+++ run.sh
@@ -4,17 +4,18 @@ type='.qcow2'
 disk=$2$type
 cmd=$1
 
-echo $disk
 
 if [ "$cmd" = "init" ]; then
+mkdir $2
 read -p "Enter disk size (30G recommend): " size
 qemu-img create -f qcow2 $disk $size
+mv $disk $2
 
 elif [ "$cmd" = "start" ];then
-qemu-system-x86_64 -hda $disk -boot d -cdrom kali-linux-2022.3-installer-amd64.iso  -m 2G -usb -machine pc
+qemu-system-x86_64 -hda $2/$disk -boot d -cdrom $3  -m 2G -usb -machine pc
 
 elif [ "$cmd" = "launch" ];then
-sudo qemu-system-x86_64 -hda $disk -boot d -m 2G -nic vmnet-bridged,ifname=en0 -usb -machine q35 -cpu max -smp cores=8,threads=1,sockets=1  &
+sudo qemu-system-x86_64 -hda $2/$disk -boot d -m 2G -nic vmnet-bridged,ifname=en0 -usb -machine q35 -cpu max -smp cores=8,threads=1,sockets=1  &
 
 elif [ "$cmd" = "list" ];then
 find . -name "*.qcow2"
